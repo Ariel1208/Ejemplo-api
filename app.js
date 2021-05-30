40127bcbe1bc55efc1b415fe361394b795eba3b5
Ariel Anguiano <arielm1208@gmail.com>
2021-05-30 13:24:56 -0500

    Metodo- Traida de usuario

--- app.js
+++ app.js
@@ -28,69 +28,69 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Api');
 })
 
-/*
-app.get('/costumers', (req, res) => {
-    const sql = 'SELECT* FROM usuario';
-    connection.query(sql, (error, results) => {
-        if (error) throw error;
-        if (results.length > 0) {
-            res.json(results);
-        } else {
-            res.send('Not resutl');
-        }
-    });
-})
 
-app.get('/costumers/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = `SELECT* FROM costumer WHERE id = ${id}`;
-    connection.query(sql, (error, results) => {
-        if (error) throw error;
-        if (results.length > 0) {
-            res.json(results);
-        } else {
-            res.send('Not resutl');
-        }
-    });
+app.get('/Usuarios', (req, res) => {
+        const sql = 'SELECT* FROM lista_servicio_cocina';
+        connection.query(sql, (error, results) => {
+            if (error) throw error;
+            if (results.length > 0) {
+                res.json(results);
+            } else {
+                res.send('Not resutl');
+            }
+        });
+    })
+    /*
+    app.get('/costumers/:id', (req, res) => {
+        const { id } = req.params;
+        const sql = `SELECT* FROM costumer WHERE id = ${id}`;
+        connection.query(sql, (error, results) => {
+            if (error) throw error;
+            if (results.length > 0) {
+                res.json(results);
+            } else {
+                res.send('Not resutl');
+            }
+        });
 
-})
-
-app.post('/add', (req, res) => {
-    const sql = 'INSERT INTO costumer SET ?';
-
-    const customerObj = {
-        name: req.body.name,
-        city: req.body.city
-    }
-
-    connection.query(sql, customerObj, error => {
-        if (error) throw error;
-        res.send('Costumer created');
     })
 
-});
+    app.post('/add', (req, res) => {
+        const sql = 'INSERT INTO costumer SET ?';
 
-app.put('/update/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, city } = req.body;
+        const customerObj = {
+            name: req.body.name,
+            city: req.body.city
+        }
 
-    const sql = `UPDATE costumer SET name = '${name}', city = '${city}' WHERE id = ${id}`;
+        connection.query(sql, customerObj, error => {
+            if (error) throw error;
+            res.send('Costumer created');
+        })
 
-    connection.query(sql, (error, results) => {
-        if (error) throw error;
-        res.send('Costumer updated')
     });
 
+    app.put('/update/:id', (req, res) => {
+        const { id } = req.params;
+        const { name, city } = req.body;
 
-})
+        const sql = `UPDATE costumer SET name = '${name}', city = '${city}' WHERE id = ${id}`;
 
-app.delete('/delete/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = `DELETE FROM costumer  WHERE id = ${id}`;
+        connection.query(sql, (error, results) => {
+            if (error) throw error;
+            res.send('Costumer updated')
+        });
 
-    connection.query(sql, (error, results) => {
-        if (error) throw error;
-        res.send('Costumer updated')
-    });
 
-});*/
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        const { id } = req.params;
+        const sql = `DELETE FROM costumer  WHERE id = ${id}`;
+
+        connection.query(sql, (error, results) => {
+            if (error) throw error;
+            res.send('Costumer updated')
+        });
+
+    });*/

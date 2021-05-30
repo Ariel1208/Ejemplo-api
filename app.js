dd6803c2b6f9bae6367d8d39aa1c78976bf96470
Ariel Anguiano <arielm1208@gmail.com>
2021-05-30 14:20:25 -0500

    cambios

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const PORT = process.env.PORT || 3050;
 
 const app = express();
 
-app.use(
-    (req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-        next();
-    }
-);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+    bodyParser.json()
+});
 
 var connection = mysql.createPool({
     host: 'us-cdbr-east-04.cleardb.com',

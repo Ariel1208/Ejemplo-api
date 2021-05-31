3c369f8dca31c9a2636b649309737d8d64300097
Ariel1208 <arielm1208@gmail.com>
2021-05-31 13:13:05 -0500

    CAMBIOS

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3050;
 
 const app = express();
-app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    bodyParser.json()
     next();
 });
 

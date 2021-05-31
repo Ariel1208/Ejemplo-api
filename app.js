36bb74cba4cd5e7f072ff0015ea13f93b90f0127
Ariel1208 <arielm1208@gmail.com>
2021-05-31 13:35:18 -0500

    Cabeceras

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3050;
 
-const app = express();
+const app = express();s
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });

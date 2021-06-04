2899603f1d162f141069ced16ced0570954efa14
Ariel1208 <arielm1208@gmail.com>
2021-06-04 12:33:02 -0500

    asfdgsad

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ const PORT = process.env.PORT || 3050;
 const app = express();
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-},  bodyParser.json());
+app.use(cors,  bodyParser.json());
 
 
 /*app.use(

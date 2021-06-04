77bd43c26e90ff530fe90169e19b4213a198419c
Ariel1208 <arielm1208@gmail.com>
2021-06-04 11:30:17 -0500

    Correccion de headers

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ const app = express();
 
 
 app.use(
-    cors(),
+    res.header('Access-Control-Allow-Origin', '*'),
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method'),
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'),
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE'),
+    next(),
     bodyParser.json()
     );
 
